6e035d9791f62ea3771e93d3f8c821340779c52a
shintosp <50572105+shintosp@users.noreply.github.com>
2020-07-16 17:56:16 +0530

    Add files via upload

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,14 +1,17 @@
 <template>
     <div class="container">
-        <input type="text" value="hello" class="inputBox" placeholder="Enter Text" v-model="newTask"/>
+        <input type="text" value="hello" class="inputBox" placeholder="Enter Task" v-model="newTask"/>
         <button @click="addNewTask">+ Add</button>
         <div class="display-box">
             <div v-for="(task,index) in tasks" :key="task.id">
                 <span v-if="!task.isEditable">{{task.taskName}}</span>
                 <input v-else type="text" @keyup.enter="editComplete(task)" v-model="editName"  /> 
+                <input type="checkbox" v-model="task.isCompleted" /> 
                 <span class="task-button" @click="editTask(task)">Edit</span>
                 <span class="task-button" @click="deleteTask(index)">Delete</span>
             </div>
+            <button @click="tasks = []">Delete All</button>
+            <button @click="sortTasks">Sort</button>
         </div>
     </div>
 </template>
@@ -16,9 +19,9 @@
 <script>
     export default {
         name: "TodoList",
-        editedNamed: "",
         data() {
             return {
+                editedName: "",
                 newTask: "",
                 taskId: 1,
                 tasks: []
@@ -26,14 +29,16 @@
         },
         methods: {
             addNewTask: function() {
-                this.tasks.push({
-                    id: this.taskId,
-                    taskName: this.newTask,
-                    isCompleted: false,
-                    isEditable: false 
-                });
-                this.newTask = "";
-                this.taskId++;
+                if(this.newTask.length>0) {
+                    this.tasks.push({
+                        id: this.taskId,
+                        taskName: this.newTask,
+                        isCompleted: false,
+                        isEditable: false 
+                    });
+                    this.newTask = "";
+                    this.taskId++;
+                }
             },
             editTask: function(task) { 
                 this.editName = task.taskName;               
@@ -45,6 +50,19 @@
             editComplete: function(task) {                
                 task.taskName = this.editName;
                 task.isEditable = false;
+            },
+            sortTasks: function() {
+                let sortedArr = [];
+                this.tasks.forEach(item => {
+                    if(item.isCompleted==true)
+                    {
+                        sortedArr.unshift(item);
+                    }
+                    else {
+                        sortedArr.push(item);
+                    }
+                })
+                this.tasks = sortedArr;
             }
         }
     }
@@ -61,7 +79,7 @@
 }
 
 .display-box {
-    max-width: 430px;
+    max-width: 480px;
     margin: 30px auto 0;
     text-align: left;
     >div {
@@ -78,6 +96,28 @@
         .task-button:hover {
             box-shadow: 1px 1px 4px rgba(0,0,0,0.4);      
         }
+    }
+}
+
+input[type="checkbox"] {
+    opacity: 0.5;
+    cursor: pointer;
+    &:hover, &:focus {
+        opacity: 1;
+    }
+}
+
+button {
+    cursor: pointer;
+    padding: 5px 10px;
+    font-size: 14px;
+    background-color: #962c2c;
+    border: none;
+    border-radius: 5px;
+    margin: 0 10px;
+    color: #fff;
+    &:hover {
+        background-color: #520303;
     }
 }
 </style>
